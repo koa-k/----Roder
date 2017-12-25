53933e8695604f1cf92b42bda491746401b77626
C912587341 <34635880+C912587341@users.noreply.github.com>
2017-12-25 16:15:21 +0800

    Add files via upload

---
+++ searchtheway.cpp
@@ -0,0 +1,46 @@
+void searchtheway(Graph &g,x,y,dist[],path[])
+{
+	int n = g.Max_vertexs;
+	int i,j,m;
+	int min;
+	int a[n] = {0};
+	for(i = 0;i<n;i++)
+	{
+		if(i != x)
+		{
+			dist[i] == g.get_length(x,i);
+			if(dist[i] < 16)
+			   path[i] = v;
+			else
+			   path[i] = -1;
+		}
+	}
+	dist[v] = 0;
+	a[v] = 1;
+	for(j = 0;i<n-1;j++)
+	{
+		min = 16;
+	    m = v;
+		for(int k = 0;k<n;k++)
+		{
+			if(dist[k]<min && a[k] == 0)
+			{
+				min = dist[k];
+				m = k;
+			}
+		}
+		a[k] = 1;
+		for(int l = 0;l<n;l++)
+		{
+			int z = g.get_length(m,l);
+			if(a[l] == 0&&dist[l]>min+z)
+			{
+				dist[l] = min + z;
+				path[l] = m;
+			}
+			   
+		}
+		
+	}
+	//Êä³ö 
+}
