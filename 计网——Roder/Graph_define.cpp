3412015c4ae8d43f4ab1756386e5417df50f77b7
oforange <34823785+oforange@users.noreply.github.com>
2017-12-25 16:15:30 +0800

    图
    图定义

---
+++ 计网——Roder/Graph_define.cpp
@@ -0,0 +1,46 @@
+#include"Graph.h"
+
+int Graph::get_length(int a, int b){
+	return ptr[a][b];
+}
+
+void Graph::add_Edges(){
+	int count1 = 0;
+	int count2 = 0;
+	fstream infile("Graph.txt", ios::in);
+	if (!infile){
+		cerr << "open error!" << endl;
+		exit(1);
+	}
+	ptr = new int*[Max_vertexs];
+	while (count1<Max_vertexs){
+		ptr[count1] = new int[Max_vertexs];
+		count1++;
+	}
+	for (count1 = 0; count1 < Max_vertexs; count1++){
+		for (; count2 < Max_vertexs; count2++){
+			infile >> ptr[count1][count2];
+		}
+	}
+}
+
+void Graph::get_Max_vertexs(){
+	fstream infile("Graph.txt", ios::in);
+	if (!infile){
+		cerr << "open error!" << endl;
+		exit(1);
+	}
+	while (!infile.eof()){
+		Max_vertexs++;
+	}
+	Max_vertexs = sqrt(Max_vertexs);
+}
+
+Graph::Graph()
+{
+}
+
+Graph::~Graph()
+{
+	delete []ptr;
+}
