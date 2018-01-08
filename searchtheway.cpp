3bd93e1beb281d6cfc9a9ea5ca503e62d05dd727
foXerw <34823097+foXerw@users.noreply.github.com>
2018-01-08 10:58:31 +0800

    Update searchtheway.cpp

--- searchtheway.cpp
+++ searchtheway.cpp
@@ -3,6 +3,7 @@ void Graph::searchtheway(Graph &g,int v,int dist[],int path[])
 {
 	int n = g.Max_vertexs;
 	int i,j,m;
+	int k;
 	int min;
 	int a[n] = {0};
 	for(i = 0;i<n;i++)
@@ -21,16 +22,16 @@ void Graph::searchtheway(Graph &g,int v,int dist[],int path[])
 	for(j = 0;i<n-1;j++)
 	{
 		min = 16;
-	    m = v;
-		for(int k = 0;k<n;k++)
+	        m = v;
+		for(k = 0;k<n;k++)
 		{
 			if(dist[k]<min && a[k] == 0)
 			{
 				min = dist[k];
 				m = k;
 			}
-			a[k] = 1;
 		}
+		a[k] = 1;
 		for(int l = 0;l<n;l++)
 		{
 			int z = g.get_length(m,l);
