b6e44220b835c505a9ae83bd95a16370643ef324
Roder <34823785+oforange@users.noreply.github.com>
2018-01-08 10:21:47 +0800

    Add files via upload

---
+++ Graph.h
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<fstream>
+#include<math.h>
+using namespace std;
+class Graph
+{
+public:
+	int get_length(int a,int b);	//获得结点间的距离
+	void add_Edges();				//从文件读入邻接矩阵
+	int Max_vertexs;				//存储点的最大数目
+	void get_Max_vertexs();			//获取点的最大数目
+	void inc_Edges(int x);
+	Graph();
+	~Graph();
+
+private:
+	int **ptr;
+};
