16e3270d6d42b3c88e8f3cda7d623704f44d3365
oforange <34823785+oforange@users.noreply.github.com>
2018-01-08 13:51:46 +0800

    ..

--- Graph.h
+++ Graph.h
@@ -9,6 +9,7 @@ public:
 	void add_Edges();				//从文件读入邻接矩阵
 	int Max_vertexs;				//存储点的最大数目
 	void get_Max_vertexs();			//获取点的最大数目
+	const int Max_dis = 16;
 	void inc_Edges(int x);
 	void searchtheway(Graph &g, int v, int dist[], int path[]);
 	void printpath(Graph &g,int dist[],int path[], int v);
